93be2c9ed6f82cfc48661135b63692bfb7887376
frisaac <frisaac@stu.naperville203.org>
2018-08-14 19:03:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "My name is faith.<br>
+    My hometown is naperville.<br>
+    My favorite song is "
+  end
 end
